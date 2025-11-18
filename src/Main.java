014650a3a8be19576b4d89fc7e7a5e7462ead1c4
Nat Nats <vargasnatnat10@gmail.com>
2025-11-18 20:05:20 +0800

    Massive changes

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,14 @@ public class Main {
 
         /* Another task, improve the output of the Search function when doing output ]
         *  Add blank space into Search function by keyword
+        *
+        * Another task to accomplish -> Add a file writer and file reader File name of the .txt file should be
+        *
+        * fileName -> Class
+        *
+        * items.txt -> InventoryManager
+        * request.txt -> staffRequestManager
+        * user.txt -> UserManager
         * */
     }
 }
