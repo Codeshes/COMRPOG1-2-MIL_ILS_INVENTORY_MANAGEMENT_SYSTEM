83c0af435077c30a13558e178f5744f851f6dad0
Nat Nats <vargasnatnat10@gmail.com>
2025-11-19 19:12:15 +0800

    Request.java - Created the regEx fromFile method, for the notepad Database. In order to get the array sorting arrangement list.

--- src/Request.java
+++ src/Request.java
@@ -1,20 +1,30 @@
 public class Request {
     private static int counter = 100;
     private final int id;
-    private final int itemId;
+    private final String itemName;
     private double proposedPrice;
     private final String reason;
 
 
-    Request(int itemId, double proposedPrice, String reason) {
+    Request(String itemName, double proposedPrice, String reason) {
         this.id = counter++;
-        this.itemId = itemId;
+        this.itemName = itemName;
         this.proposedPrice = proposedPrice;
         this.reason = reason;
     }
 
+    Request (int id, String itemName, double proposedPrice, String reason ) {
+        this.id = id;
+        this.itemName = itemName;
+        this.proposedPrice = proposedPrice;
+        this.reason = reason;
+
+        if (id >= counter) counter = id + 1;
+
+    }
+
     public int getId() { return  id; }
-    public int getItemName() { return itemId; }
+    public String getItemName() { return itemName; }
     public double getProposedPrice() { return proposedPrice; }
     public String getReason() { return reason;}
 
@@ -24,9 +34,26 @@ public class Request {
     public String toString() {
         return "Price Change Request:" +
                 "ID = " + id +
-                ", Item name: " + itemId + '\'' +
+                ", Item name: " + itemName + '\'' +
                 ", Proposed Price: " + proposedPrice +
                 ", REASON: " + reason + '\'';
+
+    }
+
+    public static Request fromFile(String line) {
+        String[] p = line.split("\\|");
+
+        if (p.length != 4){
+            throw new IllegalArgumentException("Invalid user data format: " + line);
+        }
+
+            int itemId = Integer.parseInt(p[0].replace("ID:", "").trim());
+            String itemNAME = p[1].replace("Item name:", "").trim();
+            double proposedPrices = Double.parseDouble(p[2].replace("Proposed Price: ", "").trim());
+            String reason = p[3].replace("REASON:", "").trim();
+
+
+            return  new Request(itemId, itemNAME, proposedPrices, reason);
     }
 
 }
